168956fc2de95b91ac3d94820d085350fe3ce019
shengyang <123456>
2016-09-29 14:40:37 +0800

    vue2.0 webpack element

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
+Vue.use(Element)
 /* eslint-disable no-new */
 new Vue({
-  el: 'body',
-  components: { App }
+  el: '#app',
+  render: h => h(App)
 })
